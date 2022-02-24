7db6902641cf379f9c800fdff6f351a1d83f9433
JulienL-Github <julienlecornu@gmail.com>
2022-02-24 12:54:07 +0100

    Test PHP

---
+++ header.php
@@ -0,0 +1,12 @@
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <a href="index.html"><label class="logoNav"></label></a>
+    <ul>
+            <li><a href="#partenaires">Partenaire</a></li>
+            <li><a href="calendrier.html">Contact</a></li>
+            <li><a href="#reseauxSociaux">Reseaux Sociaux</a></li>
+    </ul>
+</nav>
